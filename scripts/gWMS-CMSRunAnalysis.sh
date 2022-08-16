3d432c1da1ad0ab249fb5b0f75d25d6f4b418386
Dario Mapelli <mapelli.dario@gmail.com>
2022-08-16 18:30:11 +0200

    jobwr - copy/paste from WMCore - part6 - bug

--- scripts/gWMS-CMSRunAnalysis.sh
+++ scripts/gWMS-CMSRunAnalysis.sh
@@ -12,12 +12,15 @@
 # it. Be aware, that there are some read-only variables, like: BASHOPTS, BASH_VERSINFO,
 # EUID, PPID, SHELLOPTS, UID, etc.
 
-# CRAB
-set > startup_environment.sh
-sed -e 's/^/export /' startup_environment.sh > tmp_env.sh
-mv tmp_env.sh startup_environment.sh
-# WMCore new, broken?
-#set | sed 's/^/export /g' > startup_environment.sh
+# # CRAB
+# set > startup_environment.sh
+# sed -e 's/^/export /' startup_environment.sh > tmp_env.sh
+# mv tmp_env.sh startup_environment.sh
+# WMCore
+# these lines are required by src/python/WMCore/Storage/Backends/GFAL2Impl.py !!
+set | sed 's/^/export /g' > startup_environment.sh
+export JOBSTARTDIR=$PWD
+
 
 # Saving START_TIME and when job finishes, check if runtime is not lower than 20m
 # If it is lower, sleep the difference. Will not sleep if CRAB3_RUNTIME_DEBUG is set.
